6045ea308254ac416a7811be786bf7c1d721f2a7
Zoe Ames <zoeames@gmail.com>
2015-02-02 19:44:31 -0600

    square drawn in canvas

--- public/main.js
+++ public/main.js
@@ -1,5 +1,28 @@
 var c = document.getElementById('myCanvas');
 var ctx = c.getContext('2d');
+
+//bottom
 ctx.beginPath();
-ctx.arc(95,50,40,0,2*Math.PI);
-ctx.stroke();
+ctx.moveTo(0, 200);
+ctx.lineTo(200, 200);
+ctx .stroke();
+
+//top
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 0);
+ctx .stroke();
+
+
+//right side
+ctx.beginPath();
+ctx.moveTo(200, 0);
+ctx.lineTo(200, 200);
+ctx .stroke();
+
+
+//left side
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(0, 200);
+ctx .stroke();
